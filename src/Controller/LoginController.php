b534972b2ea855a18a2f2d975f193a5115bff435
Gugawa <zd46778@zut.edu.pl>
2023-11-09 15:12:13 +0100

    lab-e security, roles

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // jezeli wystepuje blad logowania, wyswietl
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // osatni wpisany uzytkownik
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+}
